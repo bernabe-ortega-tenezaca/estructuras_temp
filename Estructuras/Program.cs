e35789e83900e0865757d315ab53cc18d2fc048c
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-01-28 19:08:21 -0500

    first commit

---
+++ Estructuras/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("                     Universidad Estatal Amazónica");
+
+Console.WriteLine("Imprimir los elementos de una cola");
+//Queue sirve para definir una cola
+Queue<string> nombres = new Queue<string>(); //nombres es una colección
+
+nombres.Enqueue("Ana"); // primero en entrar primero en salir  FIFO=>Cola
+nombres.Enqueue("Luis");
+nombres.Enqueue("Lucia");
+nombres.Enqueue("Carlos");
+
+nombres.Dequeue();
+
+foreach (var item in nombres) //foreach recorre todos y cada uno de los elementos de una colección
+{
+    Console.WriteLine(item);
+}
+
+
+

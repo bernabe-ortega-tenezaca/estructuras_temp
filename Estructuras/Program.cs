90594b9e0c594ae9ed3e7f63b230e6d675723a20
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-02-11 18:55:35 -0500

    Crear archivo de conjuntos

--- Estructuras/Program.cs
+++ Estructuras/Program.cs
@@ -1,37 +1,6 @@
 ﻿System.Console.WriteLine("              Universidad Estatal Amazónica");
 System.Console.WriteLine();
-Console.WriteLine("Semana 9 - Conjuntos Tutoria Martes");
-System.Console.WriteLine("================================");
 
-//Definir un conjunto llamado miconjunto
-HashSet<string> miconjunto = new HashSet<string>();
-miconjunto.Add("Ana");
-miconjunto.Add("José");
-miconjunto.Add("Ana"); // Los conjuntos no aceptan elementos repetidos
-
-foreach (var item in miconjunto)
-{
-    System.Console.WriteLine(item);
-}
-
-//Buscar el elemento Ana
-//método 1
-bool encontrado = false;
-
-encontrado = miconjunto.Contains("ana");
-if(encontrado == true){
-    System.Console.WriteLine("El elemento si existe");
-}else{
-    System.Console.WriteLine("Elemento no encontrado");
-}
-
-//Buscar el elemento José
-//métddo 2
-if(miconjunto.Contains("José")){
-    System.Console.WriteLine("Encontrado");
-}else{
-    System.Console.WriteLine("No encontrado");
-}
-
+//ConjuntosMartes.run();
 
 
--- Estructuras/obj/Debug/net8.0/Estructuras.AssemblyInfo.cs
+++ Estructuras/obj/Debug/net8.0/Estructuras.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35789e83900e0865757d315ab53cc18d2fc048c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8064aa1d3c1b0408a061f2b0632c3cfdf089c8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

2baeae923bf0b03e322de792cd9de02648a853d3
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-02-11 18:59:30 -0500

    Union de conjuntos

--- Estructuras/Program.cs
+++ Estructuras/Program.cs
@@ -2,5 +2,14 @@
 System.Console.WriteLine();
 
 //ConjuntosMartes.run();
+HashSet<int> numeros = new HashSet<int>{1,2,3};
+HashSet<string> nombres = new HashSet<string>{"Ana", "Juan", "Pedro"};
+HashSet<string> apellidos = new HashSet<string>{"Ana", "Perez", "Tiago"};
 
+nombres.UnionWith(apellidos);
+
+foreach (var item in nombres)
+{
+    System.Console.WriteLine(item);
+}
 

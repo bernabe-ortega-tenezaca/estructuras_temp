90a8f4f9fdef2a95bbc4956f3614fafc9e068c12
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-02-11 19:04:10 -0500

    Union e interseccion

--- Estructuras/Program.cs
+++ Estructuras/Program.cs
@@ -6,9 +6,18 @@ HashSet<int> numeros = new HashSet<int>{1,2,3};
 HashSet<string> nombres = new HashSet<string>{"Ana", "Juan", "Pedro"};
 HashSet<string> apellidos = new HashSet<string>{"Ana", "Perez", "Tiago"};
 
-nombres.UnionWith(apellidos);
+// nombres.UnionWith(apellidos);
 
-foreach (var item in nombres)
+// foreach (var item in nombres)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+HashSet<string> auxiliar = new HashSet<string>();// auxiliar no tiene elementos
+
+auxiliar.UnionWith(apellidos); // hago la union de auxiliar con apellidos 
+auxiliar.IntersectWith(nombres); // interseccion de auxiliar(apellidos) con los nombres
+foreach (var item in auxiliar)
 {
     System.Console.WriteLine(item);
 }
--- Estructuras/obj/Debug/net8.0/Estructuras.AssemblyInfo.cs
+++ Estructuras/obj/Debug/net8.0/Estructuras.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90594b9e0c594ae9ed3e7f63b230e6d675723a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baeae923bf0b03e322de792cd9de02648a853d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructuras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
